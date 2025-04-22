8d5ddcdc1e539aab7958a9cb40b8f21de8826fb9
Andrei Lubochnikov <a.lubovhnikov@admitad.pro>
2025-04-22 20:50:41 +0530

    chore: initial project structure with health check endpoint

---
+++ cmd/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OK"))
+	})
+
+	log.Println("Server started on :8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
